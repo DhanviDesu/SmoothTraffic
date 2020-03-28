064c9b95f75018593ecbd04f8fe328b24ea58db2
Alvin Wang <alvinwang922@gmail.com>
2020-03-28 15:58:14 -0700

    Added cars to crash with

--- traffic.py
+++ traffic.py
@@ -48,7 +48,7 @@ class Traffic:
             if(i==self.target):
                 pass
             elif(temp.lane == tarCar.lane):
-                if(abs(temp.x - tarCar.x)<=0.1):
+                if(abs(temp.x - tarCar.x)<=0.25):
                     return True
         return False
 
@@ -57,18 +57,18 @@ if __name__ == '__main__':
     carArray=[]
 
     #first car
-    x1 = 10.
-    v1 = 70
-    lane1 = 3
-    car1 = Car(x1,v1,lane1,[(x1,lane1)])
-    carArray.append(car1)
+    #x1 = 10.
+    #v1 = 70
+    #lane1 = 3
+    #car1 = Car(x1,v1,lane1,[(x1,lane1)])
+    #carArray.append(car1)
 
     #second car
-    x2 = 20.
-    v2 = 65
-    lane2 = 2
-    car2 = Car(x2,v2,lane2,[(x2,lane2)])
-    carArray.append(car2)
+    #x2 = 20.
+    #v2 = 65
+    #lane2 = 2
+    #car2 = Car(x2,v2,lane2,[(x2,lane2)])
+    #carArray.append(car2)
 
     #third car
     x3 = 0.
@@ -77,38 +77,72 @@ if __name__ == '__main__':
     car3 = Car(x3,v3,lane3,[(x3,lane3)])
     carArray.append(car3)
 
-    #fourth car collides in lane 0
-    x4 = 0.
-    v4 = 66
-    lane4 = 0
-    car4 = Car(x4,v4,lane4,[(x4,lane4)])
-    carArray.append(car4)
+    #fourth car collides with third car in lane 0
+    #x4 = 66
+    #lane4 = 0
+    #car4 = Car(x4,v4,lane4,[(x4,lane4)])
+    #carArray.append(car4)
 
-    #fifth car collides with fourth car due to different speeds
-    x5 = 5.
-    v5 = 55
-    lane5 = 0
-    car5 = Car(x5,v5,lane5,[(x5,lane5)])
-    carArray.append(car5)
+    #fifth car collides with third car after travelling some distance in lane due to speed differences
+    #x5 = 5.
+    #v5 = 55
+    #lane5 = 0
+    #car5 = Car(x5,v5,lane5,[(x5,lane5)])
+    #carArray.append(car5)
 
-    #sixth car collides in lane 2
-    x6 = 0.
-    v6 = 80
-    lane6 = 2
-    car6 = Car(x6,v6,lane6,[(x6,lane6)])
-    carArray.append(car6)
+    #sixth car collides with car 3 in lane 2
+    #x6 = 0.
+    #v6 = 75
+    #lane6 = 2
+    #car6 = Car(x6,v6,lane6,[(x6,lane6)])
+    #carArray.append(car6)
 
-    #seventh car collides in lane 1
-    x7 = 0.
-    v7 = 80
-    lane7 = 1
-    car7 = Car(x7,v7,lane7,[(x7,lane7)])
-    carArray.append(car7)
+    #seventh car collides with car 3 in lane 1
+    #x7 = 0.
+    #v7 = 70
+    #lane7 = 1
+    #car7 = Car(x7,v7,lane7,[(x7,lane7)])
+    #carArray.append(car7)
+
+    #eigth car collides with car 3 in lane 3
+    x8 = 0.
+    v8 = 75
+    lane8 = 3
+    car8 = Car(x8,v8,lane8,[(x8,lane8)])
+    carArray.append(car8)
+
+    #ninth car so there is a third car to test with
+    x9 = 0.
+    v9 = 60
+    lane9 = 1
+    car9 = Car(x9,v9,lane9,[(x9,lane9)])
+    carArray.append(car9)
+
+    #tenth car
+    x10 = 0.
+    v10 = 60
+    lane10 = 0
+    car10 = Car(x10,v10,lane10,[(x10,lane10)])
+    carArray.append(car10)
+
+    #eleventh car
+    #x11 = 0.
+    #v11 = 80
+    #lane11 = 4
+    #car11 = Car(x11,v11,lane11,[(x11,lane11)])
+    #carArray.append(car11)
+
+    #twelfth car
+    #x12 = 0.
+    #v12 = 80
+    #lane12 = 3
+    #car12 = Car(x12,v12,lane12,[(x12,lane12)])
+    #carArray.append(car12)
 
     #together
     # 0 indexed target
     # car 3 is target
-    bigTraffic = Traffic(carArray,2,(100.,0))
+    bigTraffic = Traffic(carArray,0,(100.,0))
     for i in range(62):
         bigTraffic.updateOne(False,-1)
         check = bigTraffic.updateAll(0.05)
